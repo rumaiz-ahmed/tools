9d32f3bf21be68dcac83cde564e0c8a3ede23b67
Rumaiz Ahmed <rumaizahmed1@gmail.com>
2026-02-25 15:53:41 -0500

    add: clouflare integration and deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vite';
+import { devtools } from '@tanstack/devtools-vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import viteReact from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { cloudflare } from '@cloudflare/vite-plugin';
 
 const config = defineConfig({
   plugins: [
@@ -14,7 +15,8 @@ const config = defineConfig({
     tailwindcss(),
     tanstackStart(),
     viteReact(),
+    cloudflare({ viteEnvironment: { name: 'ssr' } }),
   ],
-})
+});
 
-export default config
+export default config;
